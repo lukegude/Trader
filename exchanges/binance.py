8819d146cb54182c7c0f579b6c95702ab29be847
Luke Gude <lukegudeios@gmail.com>
2022-08-02 20:37:45 -0400

    Added ML Research, base exchange skeleton

--- exchanges/binance.py
+++ exchanges/binance.py
@@ -1,10 +1,20 @@
 from exhange import Exchange
-
+import ccxt
 class BinanceAPI(Exchange):
-    def __init__(self):
+    def __init__(self, exchange=None):
         super().__init__()
+        self.exchange = exchange if exchange else ccxt.binanceus()
+        self.exchange.apiKey = self.api
+        self.exchange.secret = self.secret_api
+        self.wallet = {'BTC': self.getBTCWallet(), 'USD': self.getUSDWallet()}
 
+    def getBTCWallet(self):
+        return self.exchange.fetch_balance()['BTC']['free']
+
+    def getUSDWallet(self):
+        return self.exchange.fetch_balance()['USD']['free']
 
 
 
 b = BinanceAPI()
+print(b.wallet)
--- exchanges/exhange.py
+++ exchanges/exhange.py
@@ -7,13 +7,15 @@ class Exchange:
     def __init__(self, exchange=None):
         try:
             self.api = self.GET_API_KEY()['API-KEY']
+            self.secret_api = self.GET_API_KEY()['SECRET-API-KEY']
         except Exception as e:
             print(e)
             exit()
 
 
+
     def GET_API_KEY(self):
-        with open(os.getcwd() + '/config/config.json') as f:
+        with open(os.getcwd() + '/config/personal_config.json') as f:
             keys = json.load(f)
             if not keys['API-KEY']:
                 raise Exception('No API-KEY found in config.json')
@@ -21,4 +23,3 @@ class Exchange:
 
 
 e = Exchange()
-print(e.api)
